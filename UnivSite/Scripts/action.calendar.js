bf97665bba5f202a766c93b7c1388afa4696748c
Matusevich-Yevgenii <zhenya.matusevich@gmail.com>
2016-02-21 22:50:45 +0200

    added callendar

---
+++ UnivSite/Scripts/action.calendar.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function() {
+    $("#jqxcalendar").jqxCalendar({ width: '800px', height: '700px' });
+    $('#jqxcalendar').bind('valuechanged', function(event) {
+        var date = event.args.date;
+        $("#log").text(date.toDateString());
+    });
+});
