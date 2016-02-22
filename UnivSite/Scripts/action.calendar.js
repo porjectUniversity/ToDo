df34d740c4e6b73bb71d3828c5050559b5d620d3
Matusevich-Yevgenii <zhenya.matusevich@gmail.com>
2016-02-22 15:25:29 +0200

    added left menu-scroll

--- UnivSite/Scripts/action.calendar.js
+++ UnivSite/Scripts/action.calendar.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function() {
-    $("#jqxcalendar").jqxCalendar({ width: '800px', height: '700px' });
+    $("#jqxcalendar").jqxCalendar({ width: '780px', height: '600px' });
     $('#jqxcalendar').bind('valuechanged', function(event) {
         var date = event.args.date;
         $("#log").text(date.toDateString());
